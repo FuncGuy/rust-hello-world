acf58e4a32f9aeb9796734f7a61c0d037c281117
Tirumalesh <49618191+FuncGuy@users.noreply.github.com>
2023-12-01 18:58:45 +0530

    rust

--- hello.rs
+++ hello.rs
@@ -1,3 +1,9 @@
+fn print_number(number: &mut i32) {
+  *number += 1;
+  println!("The number is: {}", number);
+}
+
 fn main() {
-  println!("Hello world!");
+  let mut x = 42;
+  print_number(&mut x);  // Taking an immutable reference to x
 }
